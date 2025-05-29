0eb2fc31089d27aa419b400e05576493fe690ba3
BohdanDushyn <125987640+BohdanDushyn@users.noreply.github.com>
2025-05-29 21:40:12 +0200

    Revert "Animations"

--- Source/PhantonShade/PhantonShade.Build.cs
+++ Source/PhantonShade/PhantonShade.Build.cs
@@ -8,7 +8,7 @@ public class PhantonShade : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ProceduralMeshComponent" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
